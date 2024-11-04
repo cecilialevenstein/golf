802f76c8614916d0ffb490c641240e4c2b56b814
cecilialevenstein <c.helenalevenstein@gmail.com>
2024-11-04 10:29:50 -0300

    correcao funcao random

--- js/script.js
+++ js/script.js
@@ -1,24 +1,47 @@
 const bolinhaElemento = document.querySelector(".bolinha");
 let posicao = 0;
 
-function moverBolinha(event) {
-    if (event.keyCode == 39) {
-        posicao += 10;
-        bolinhaElemento.style.marginLeft = `${posicao}px`;
-        if(posicao == 400) {
-            setTimeout(() => {
-             alert("Parabéns você chegou!");
-        },50);
-    }
-        console.log("moverBolinha", bolinhaElemento, posicao);
-    }
-
-
+function ganhou() {
+  setTimeout(() => {
+    alert("Parabéns você chegou!");
+    reiniciar();
+  }, 50);
 }
+
+function perdeu() {
+    setTimeout(() => {
+    alert("Poxa, errou! Tente novamente!");
+    reiniciar();
+    }, 50);
+}
+
+function moverBolinha(event) {
+  if (event.keyCode == 39) {
+    posicao += 10;
+    bolinhaElemento.style.marginLeft = `${posicao}px`;
+    if (posicao == 400) {
+      ganhou();
+    } else {
+        perdeu();
+    }
+  }
+}
+
+function random() {
+    posicao = Math.random() * 400;
+    bolinhaElemento.style.marginLeft = `${posicao}px`;
+    if (posicao == 400) {
+      ganhou();
+    } else {
+      perdeu();
+    }
+  }
 
 document.addEventListener("keydown", moverBolinha);
 
-function reiniciar () {
-    posicao = 0;
-    bolinhaElemento.style.marginLeft = `${posicao}px`;
+function reiniciar() {
+  posicao = 0;
+  bolinhaElemento.style.marginLeft = `${posicao}px`;
 }
+
+
